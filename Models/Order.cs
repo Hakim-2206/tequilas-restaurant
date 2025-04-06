d5c8f94bb935bd4c4300ef9369fb9e0da294095a
Hakim.Z <110186627+Hakim-2206@users.noreply.github.com>
2025-04-06 15:03:35 +0200

    Initial commit

---
+++ Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace TequilasRestaurant.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public DateTime OrderTime { get; set; }
+
+        public string? UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
